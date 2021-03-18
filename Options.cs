c6ccb6be00177a29ca3c611d59d9becc3d2417a4
MatiArazi <MatiArazi@github.com>
2021-03-18 11:03:04 -0300

    Copy 3 part 1

---
+++ Options.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    public Toggle musicToggle;
+    public Toggle soundFXToggle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicToggle.isOn = PlayerPrefs.GetInt("Music", 1) == 1;
+        soundFXToggle.isOn = PlayerPrefs.GetInt("SoundFX", 1) == 1;
+    }
+
+
+    public void setMusic(bool isOn)
+    {
+        if(isOn) PlayerPrefs.SetInt("Music", 1);
+        else PlayerPrefs.SetInt("Music", 0);
+    }
+
+    public void setSoundFX(bool isOn)
+    {
+        if (isOn) PlayerPrefs.SetInt("SoundFX", 1);
+        else PlayerPrefs.SetInt("SoundFX", 0);
+    }
+}
